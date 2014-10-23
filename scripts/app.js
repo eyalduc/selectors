ecfa5df78fbad07e3d55582be59a55098cd46da6
unknown <I069489@TLVD60208759A.sap.corp>
2014-10-23 23:18:02 +0300

    Enhance challenges

--- scripts/app.js
+++ scripts/app.js
@@ -10,9 +10,35 @@ require(['selectors'],function(selectorsAPI){
 
 
 var handleSelector = function(text){
+    var relatedResult = "";
+    var checkboxes = $('.checkbox');
     var selector = $('#userSelector').val();
+    if(!selector){
+        alert('Please input your answer!');
+    }
+    for(var i=0; i<checkboxes.length; i++){
+        if(checkboxes[i].checked && !checkboxes[i].disabled){
+            //not so nice
+            var liParent = checkboxes[i].parentElement.parentElement.parentElement;
+            relatedResult = liParent.getAttribute('data-result');
 
-    console.log(eval(selector)); //Dangerous and slow - eval is not to be used !!!
+            if(relatedResult === selector){//check if challenge is solved
+                checkboxes[i].disabled = true;
+                $(liParent).addClass('challengeCompleted');
+                $(checkboxes[i]).parent().parent().next().next().removeClass('hidden')
+            }
+        }
+    }
+
+
+   if(!relatedResult){
+       alert('you must select the desired checkbox for your answer');
+   }
+
+
+  //  var selector = $('#userSelector').val();
+
+  //  console.log(eval(selector)); //Dangerous and slow - eval is not to be used !!!
 
     //using a function
     //var func = new Function(selector);
